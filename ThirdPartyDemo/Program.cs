de42eba79cbcf24ab07f9db46bca0203ee51773c
谢承忠 <@DESKTOP-6I7NCJK>
2019-05-24 14:35:06 +0800

    添加项目文件。

---
+++ ThirdPartyDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+
+namespace ThirdPartyDemo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
